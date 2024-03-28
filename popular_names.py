1763c1db50f3d4632e009e615417b1f55c39c7f8
“Talmage <talmage.anderson17@com>
2024-03-28 13:32:20 -0600

    added a button

--- popular_names.py
+++ popular_names.py
@@ -64,7 +64,17 @@ st.plotly_chart(fig_top10)
 #name_df.groupby('year')['n'].sum().reset_index()  # sum of people named Arthur in the year
 #fig = px.line(data_frame = name_df, x = 'year', y = 'n')
 
+# Define the label for the button
+button_label = "Visit SSA Baby Names Limits Page"
 
+# Define the URL to redirect to when the button is clicked
+url = "https://www.ssa.gov/oact/babynames/limits.html"
+
+# Create a clickable button
+if st.button(button_label):
+    # Redirect to the specified URL when the button is clicked
+    st.markdown(f"[Redirecting to {url} ...]({url})")
+    st.experimental_rerun()
 
 # https://class-app-app1-6wncbftxutalqdvzn5tsed.streamlit.app
 st.caption("Source: popular_names.csv from Social Security website")
